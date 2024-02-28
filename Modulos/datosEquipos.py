61a6b1d91e83f69bdbc59383cf8823eaf3ef9347
Adrián Uceta Gamaza <aucegam0210@g.educaand.es>
2024-02-28 19:09:58 +0100

    funcion datosEquipos finalizada

--- Modulos/datosEquipos.py
+++ Modulos/datosEquipos.py
@@ -1,11 +1,23 @@
 # Determina resultado. Devuelve 1 si gana, 0 si empata, -1 si pierde.
 def quienGana(resultado):
-    print("Por hacer")
+
+    goles_local,goles_visitante=int(resultado.split("-"))
+    
+    if goles_local==goles_visitante:
+        return 0
+    elif goles_local>goles_visitante:
+        return 1
+    else:
+        return -1
+
+
 
 # Recibe lista de partidos empatados ganados, empatados y perdidos. Devuelve el total de puntos. +3/victoria, +1/empate, 0/derrota.
-def puntos(info):
-    print("Por hacer")
+def puntos(*info):
+    
+   puntos=info[0].count*3+info[1].count
+   return puntos
 
 # Ordena la lista recibida en función de los puntos.
 def clasificacion(datos):
-    print("Por hacer")
+    return sorted(datos, key=lambda equipo: equipo[4], reverse=True)
